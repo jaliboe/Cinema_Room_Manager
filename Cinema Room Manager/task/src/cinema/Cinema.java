31ca700be05e4885e24bf5ad918176948993fde3
Jan Benjamin Böhmer <janboemer@gmx.de>
2025-02-21 00:10:00 +0100

    Added some theory from the Hyperskill course.

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -13,9 +13,18 @@ public class Cinema {
         System.out.print("Enter the number of seats in each row\n> ");
         int col = sc1.nextInt();
 
+        System.out.println();
+
         char[][] seats = new char[row][col];
 
         boolean run = true;
+        int ticketsSold = 0;
+        int seatsTotal = 0;
+        float soldPercentage = 0.0f;
+        float currentIncome = 0.0f;
+        float totalIncome = 0.0f;
+        int rowSelected = 0;
+        int colSelected = 0;
 
         for (int i = 0; i < seats.length; i++) {
             for (int j = 0; j < seats[i].length; j++) {
@@ -26,6 +35,7 @@ public class Cinema {
         while (run) {
             System.out.println("1. Show the seats");
             System.out.println("2. Buy a ticket");
+            System.out.println("3. Statistics");
             System.out.println("0. Exit");
             System.out.print("> ");
             int menuChoice = sc1.nextInt();
@@ -52,14 +62,14 @@ public class Cinema {
                     break;
                 case 2:
                     System.out.print("Enter a row number:\n> ");
-                    int rowSelected = sc1.nextInt();
+                    rowSelected = sc1.nextInt();
                     System.out.print(("Enter a seat number in that row:\n> "));
-                    int colSelected = sc1.nextInt();
+                    colSelected = sc1.nextInt();
 
                     seats[(rowSelected - 1)][(colSelected - 1)] = 'B';
 
                     int ticketPrice = 0;
-                    int seatsTotal = row * col;
+                    seatsTotal = row * col;
 
                     if (seatsTotal <= 60) {
                         ticketPrice = 10;
@@ -77,9 +87,42 @@ public class Cinema {
                         }
                     }
 
+                    ticketsSold++;
+                    soldPercentage = 100 / seatsTotal;
+                    currentIncome += ticketPrice;
+
                     System.out.print("Ticket price: $" + ticketPrice + "\n");
 
                     System.out.println();
+                    break;
+                case 3:
+
+                    for (int i = 0; i < 0; i++) {
+                        if (seatsTotal <= 60) {
+                            totalIncome += 10 * seats[i].length;
+                        } else if (row % 2 == 0) {
+                            if (i <= 5) {
+                                totalIncome = 10 * seats[i].length;
+                            } else if (i >= 6) {
+                                totalIncome = 8 * seats[i].length;
+                            }
+                        } else if (row % 2 != 0) {
+                            if (i <= 4) {
+                                totalIncome = 10 * seats[i].length;
+                            } else if (i >= 5) {
+                                totalIncome = 8 * seats[i].length;
+                            }
+                        }
+                    }
+
+                    System.out.printf("Number of purchased tickets: %d", ticketsSold);
+                    System.out.printf("%nPercentage: %.2f%%", soldPercentage);
+                    System.out.printf("%nCurrent Income: $%.0f", currentIncome);
+                    System.out.printf("%nTotal Income: $%.0f%n", totalIncome);
+
+                    System.out.println();
+
+
                     break;
                 case 0:
                     run = false;
